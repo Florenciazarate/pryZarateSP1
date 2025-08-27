2703bd938da8a5542105c720573543bb95a0e908
FlorenciaZarate <florenciazarate2110@gmail.com>
2025-08-27 13:11:51 -0300

    Agregar archivos de proyecto.

---
+++ pryZarateSP1/frmPrincipal.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace pryZarateSP1
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUsuario = new TextBox();
+            txtContraseña = new TextBox();
+            lblUsuario = new Label();
+            lblContraseña = new Label();
+            btnAceptar = new Button();
+            btnCancelar = new Button();
+            SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(168, 73);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(100, 23);
+            txtUsuario.TabIndex = 0;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Location = new Point(168, 112);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(100, 23);
+            txtContraseña.TabIndex = 1;
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Font = new Font("Candara", 12F, FontStyle.Bold);
+            lblUsuario.Location = new Point(64, 73);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(63, 19);
+            lblUsuario.TabIndex = 2;
+            lblUsuario.Text = "Usuario";
+            lblUsuario.Click += label1_Click;
+            // 
+            // lblContraseña
+            // 
+            lblContraseña.AutoSize = true;
+            lblContraseña.Font = new Font("Candara", 12F, FontStyle.Bold);
+            lblContraseña.Location = new Point(64, 112);
+            lblContraseña.Name = "lblContraseña";
+            lblContraseña.Size = new Size(88, 19);
+            lblContraseña.TabIndex = 3;
+            lblContraseña.Text = "Contraseña";
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.BackColor = Color.Tan;
+            btnAceptar.Font = new Font("Yu Gothic", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAceptar.Location = new Point(113, 186);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(85, 36);
+            btnAceptar.TabIndex = 4;
+            btnAceptar.Text = "Aceptar";
+            btnAceptar.UseVisualStyleBackColor = false;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.BackColor = Color.Tan;
+            btnCancelar.Font = new Font("Yu Gothic", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCancelar.ForeColor = SystemColors.ControlText;
+            btnCancelar.Location = new Point(221, 186);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(81, 36);
+            btnCancelar.TabIndex = 5;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = false;
+            // 
+            // frmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PapayaWhip;
+            ClientSize = new Size(383, 252);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnAceptar);
+            Controls.Add(lblContraseña);
+            Controls.Add(lblUsuario);
+            Controls.Add(txtContraseña);
+            Controls.Add(txtUsuario);
+            Name = "frmPrincipal";
+            Text = "t";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUsuario;
+        private TextBox txtContraseña;
+        private Label lblUsuario;
+        private Label lblContraseña;
+        private Button btnAceptar;
+        private Button btnCancelar;
+    }
+}
